7d2f17a7173a784d67319c4900f244df6ade5c15
Austin Mazur <mazuraustin1@gmail.com>
2023-03-30 21:38:01 +0200

    Finish iterations 1, 2, 3, 4. Missing console.log on iteration 3

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const mongoose = require('mongoose');
 
+
 // Import of the model Recipe from './models/Recipe.model.js'
 const Recipe = require('./models/Recipe.model');
 // Import of the data from './data.json'
 const data = require('./data');
 
-const MONGODB_URI = 'mongodb://localhost:27017/recipe-app';
+//connected to my mongodb
+const MONGODB_URI = 'mongodb+srv://mazuraustin1:gDytBJKVd6Vrf2c8@cluster32.wvkdomp.mongodb.net/test';
 
 // Connection to the database "recipe-app"
 mongoose
@@ -16,8 +18,21 @@ mongoose
     return Recipe.deleteMany()
   })
   .then(() => {
-    // Run your code here, after you have insured that the connection was made
-  })
+    //use this to create new recipe, passing the data from seperate file
+    Recipe.create(data[0])
+    // console.log(data[0].title);
+    return Recipe.insertMany(data)
+
+})
+.then(() => { 
+  Recipe.updateOne({ title: "Rigatoni alla Genovese" }, { duration: 100 })
+  .then(console.log('success'))
+  // query search {duration: {$eq:100}} to check
+})
+
+.then(() => {
+})
   .catch(error => {
     console.error('Error connecting to the database', error);
   });
+
